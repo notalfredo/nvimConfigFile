84ef782abbfab6f8ea9b140696decd717f1cb3dd
Alfredo <icealfredo123@gmail.com>
2023-06-26 09:36:23 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("notalfredo.remap")
+require("notalfredo.packer")
+require("notalfredo.set")
+vim.cmd('colorscheme molokai')
+vim.cmd('set tabstop=4')
+vim.cmd('set shiftwidth=4')
+vim.cmd('set expandtab')
