3ebde39d19c3a0a155cd37331a92367757059c78
Alfredo <icealfredo123@gmail.com>
2023-07-23 14:53:55 -0500

    init.lua contains better color scheme supports wgsl files supports moving lines up and down

--- init.lua
+++ init.lua
@@ -1,7 +1,23 @@
 require("notalfredo.remap")
 require("notalfredo.packer")
 require("notalfredo.set")
-vim.cmd('colorscheme molokai')
 vim.cmd('set tabstop=4')
 vim.cmd('set shiftwidth=4')
+
+require("catppuccin").setup {
+    color_overrides = {
+        mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+        },
+    }
+}
+
 vim.cmd('set expandtab')
+vim.cmd('autocmd BufNewFile,BufRead *.wgsl set filetype=wgsl')
+-- Move selected text up
+vim.api.nvim_set_keymap('v', '<S-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+-- Move selected text down
+vim.api.nvim_set_keymap('v', '<S-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.cmd('set wrap')
