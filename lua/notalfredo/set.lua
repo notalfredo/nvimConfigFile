61db2ca4bab0b2adf15105254e3d6f0c007a184b
Alfredo <icealfredo123@gmail.com>
2023-08-05 17:28:15 -0500

    more neovim config

--- lua/notalfredo/set.lua
+++ lua/notalfredo/set.lua
@@ -1,1 +1,11 @@
 vim.opt.nu = true
+
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "./.vim/undodir"
+vim.opt.undofile = true
+
+
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
